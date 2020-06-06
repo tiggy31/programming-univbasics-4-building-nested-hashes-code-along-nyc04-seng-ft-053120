49dba5665d97a23590e1a05699056d2d9a1c0b73
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-06 16:09:24 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,28 +1,34 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {:name => "Lord Montague",
+        :age => "53",
         #your key/value pairs here
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {:name => "Lady Montague",:age => "54"},
+      :hero => {:name =>"Romeo",:age => "15",:status => "alive"
         },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
+      :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
+   },
    :capulet => {
-      :patriarch => {
+      :patriarch => {:name => "Lord Capulet", :age => "50"},
+      :matriarch => {:name => "Lady Capulet", :age => "51"
+        },
+      :heroine => {:name => "Juliet",:age => "15",:status => "alive"
         #your key/value pairs here
         },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
+      :heroine_friends => [{
+                      :name => "Steven",
+                      :age => "30",
+                      :attitude =>"confused"
+      },{
+                      :name => "Nurse",
+                      :age => "44",
+                      :attitude =>"worried"}
+                      ]
    }
   }
 
